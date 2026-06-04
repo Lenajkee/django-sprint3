c9586dd8c0f6e28687f3f08cf976fb91aceb335e
Елена Колмакова <139232634+Lenajkee@users.noreply.github.com>
2026-06-04 12:19:34 +0300

    1 ревью

--- blogicum/blog/urls.py
+++ blogicum/blog/urls.py
@@ -1,12 +1,12 @@
-from django.urls import path  # type: ignore[import-untyped]
-
+from django.urls import path
 from . import views
 
-app_name: str = 'blog'
+app_name = 'blog'
 
-urlpatterns: list = [
+urlpatterns = [
     path('', views.index, name='index'),
-    path('posts/<int:post_id>/', views.post_detail, name='post_detail'),
-    path('category/<slug:category_slug>/', views.category_posts,
-         name='category_posts'),
+    path('posts/<int:pk>/', views.post_detail, name='post_detail'),
+    path('category/<slug:category_slug>/', views.category_posts, name='category_posts'),
 ]
+
+
--- blogicum/blogicum/urls.py
+++ blogicum/blogicum/urls.py
@@ -3,8 +3,7 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('blog.urls')),
-    path('posts/', include('blog.urls')),
-    path('category/', include('blog.urls')),
-    path('pages/', include('pages.urls')),
+    path('', include('blog.urls', namespace='blog')),
+    path('pages/', include('pages.urls', namespace='pages')),
 ]
+
